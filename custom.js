f652ae1955a2a70d5be188e8a4ff478aa72f61b6
brizzio <fabrizio.salvade@gmail.com>
2018-11-20 18:41:56 -0200

    updates no codigo

--- custom.js
+++ custom.js
@@ -30,7 +30,10 @@ ready(function() { alert('hello'); });
 
 var url = 'https://script.google.com/macros/s/AKfycbwfI7x2dqg4kBeKvlsv8OY-arZ5w9fK5V8RSHBpLnK8hkVtVg0x/exec'
 
-var database = {}
+var database = {};
+
+var totalProdutos = 0;
+var totalSkus = 0;
 
 window.onload = start_function;
 
@@ -55,21 +58,20 @@ function start_function(){
     .then(function (response) {
         console.log(response.data);
         database = response.data
-        
+        somaSKU();
         document.getElementById("lista-categorias").innerHTML=collapse_categorias(database.categorias)
-
+        
     })
     .catch(function (error) {
         console.log(error);
     })
     .then(function () {
         // always executed
+        
     });  
   
 
 }
-
-
 
 
 function cardsHTML(){
@@ -87,7 +89,9 @@ function collapse_categorias(cat){
 
             var produtos = 0
             var subcat = c.subcategorias.map(function(sc){
+                totalProdutos+= sc.produtosCount
                 produtos += sc.produtosCount
+                
                 return `<a href="#" onclick="showProdutos('`+ c.descricao + `','`+ sc.descricao + `');" class="collection-item"><span class="new badge grey" data-badge-caption="produtos CONSTRUE">` + sc.produtosCount + `</span>`+ sc.descricao + `</a>`
             })    
 
@@ -103,11 +107,12 @@ function collapse_categorias(cat){
             </li>
            `
         })
-
+    console.log(totalProdutos)    
+    document.getElementById("total-geral").innerText = totalProdutos + " produtos"  
     return  liarray.join() 
 }
 
-function getProdutos(subcategoria) {
+/* function getProdutos(subcategoria) {
  
     database.categorias.forEach(function(c){
         c.subcategorias.forEach(function(s){
@@ -117,17 +122,84 @@ function getProdutos(subcategoria) {
             }
         })
     })
-    
-    
-} 
+  
+}  */
 
-function montaCardDeProduto(produto){
+function montaCardsDeProdutos(produtos){
    
+      return  produtos.map(function(p){
+                    var tagContexto, tagQualificacao
+                    tagContexto = JSON.stringify(p.tagCon)
+                        if(Object.getOwnPropertyNames(p.tagCon).length === 0){
+                            tagContexto = `<p>Não existem tags de contexto para esse produto...`
+                        }
+                        tagQualificacao = JSON.stringify(p.tagQual)
+                        if(Object.getOwnPropertyNames(p.tagQual).length === 0){
+                            tagQualificacao = `<p>Não existem tags de qualificação para esse produto...`
+                        }
+                        console.log(p)
+                        console.log(tagContexto)
+                        console.log(tagQualificacao)
+
+                        return `
+                        <div class="row">
+                            <div class="col s12">
+                                <div class="card blue-grey darken-1">
+                                    <div class="card-content white-text">
+                                        <span class="card-title">` + p.descricao + `</span>
+                                        <p>`+ tagContexto + `</p>
+                                        <p>`+ tagQualificacao + `</p>
+                                    </div>
+                                    <div class="card-action">
+                                        <a href="#">` + p.skus[0].length + ` SKUS</a>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>`
+                }).join();
+    
+ 
 
 }
 
+    //calcula o numero de skus existentes
+    function somaSKU() {
+        new Promise(
+            function(resolve,reject){
+                console.log("vai calcular sku")
+                var ssk = 0;
+                database.categorias.forEach(function(c){
+                    c.subcategorias.forEach(function(s){
+                        s.produtos.forEach(function(p){
+                            ssk +=p.skus[0].length
+                        })
+                    })
+                })
+                resolve(ssk)
+            }
+        )
+            .then(function (resolved) {
+                // yay, you got a new phone
+                console.log(resolved);
+                totalSkus = resolved
+                document.getElementById("total-geral-skus").innerText = resolved + " SKUS"
+            })
+            .catch(function (rejected) {
+                // ops, mom don't buy it
+                console.log(rejected.message);
+            });
+    };
+    
+
+
+    //******** fim calculo skus *********/
+
+
+
 function showProdutos(categoria, subcategoria){
     console.log("cliquei na subcategoria " + subcategoria)
+    var prodCount = 0;
     var div = document.getElementById("subcat-detail")
     document.getElementById("lista-categorias").classList.add("hide");
     div.classList.remove("hide");
@@ -148,20 +220,50 @@ function showProdutos(categoria, subcategoria){
     </nav>
     
     <div class="row">
-        <div class="col s8"><H3>` + subcategoria + `</H3></div>
+        <div class="col s9"><H3>` + subcategoria + `</H3></div>
+        <div class="col s3"><H5 id="total-produtos" class="blue-text text-darken-2"></H5></div>
         
     </div>
     `
+    var p = new Promise(
+        function(resolve,reject){
+            database.categorias.forEach(function(c){
+                c.subcategorias.forEach(function(s){
+                    if(s.descricao === subcategoria){
+                        console.log("na promessa > ")
+                        console.log(s.produtos)
+                        if(s.produtos.length > 0){
+                            // not empty 
+                            prodCount = s.produtosCount;
+                            resolve(s.produtos)
+                        } else {
+                            // empty
+                            var htmlNoProdutos = `<span class="new badge" >Não existem produtos CONSTRUE para essa subcategoria.</span>`
+                            reject(htmlNoProdutos)
+                        }
+                    }
+                })
+            })
+  
+        }
     
-    div.innerHTML=detalhe  + `<div class="row"><p>` + "teste" + `</p></div>`
+    ).then(function(resolved){
+            console.log("no then");
+            console.log(resolved)
+            div.innerHTML= detalhe + montaCardsDeProdutos(resolved);
+            document.getElementById("total-produtos").innerHTML =  prodCount + "  Produtos Construe"
+            
+    })
+    .catch(function(rejected){
+         return div.innerHTML=detalhe + rejected;
+    });
+
+}  
     
-}
 
 function visualizaInicio(){
     document.getElementById("lista-categorias").classList.remove('hide');
     document.getElementById("subcat-detail").classList.add('hide');
-
-    
 }
 
 
